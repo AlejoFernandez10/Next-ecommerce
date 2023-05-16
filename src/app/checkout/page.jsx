85a2e4d5ebebdea771bb03d64c949c0c4f61e1c2
AlejoFernandez10 <alejofernandez1239@gmail.com>
2023-05-16 19:28:33 -0300

    Checkout component added, still not functional

---
+++ src/app/checkout/page.jsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+import React from 'react'
+import Checkout from './Checkout'
+
+const page = () => {
+  return (
+  
+    <>
+      <Checkout />
+    </>
+
+  )
+}
+
+export default page
--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -7,6 +7,7 @@ import {AiOutlineShoppingCart} from 'react-icons/ai'
 import { Context } from '@/context/CartContext'
 import Image from 'next/image'
 import { toast } from 'react-toastify'
+import Link from 'next/link'
 
 const Cart = () =>{
 
@@ -167,12 +168,13 @@ const Cart = () =>{
                         </div>
                         <p className="mt-0.5 text-sm text-gray-500">Shipping and taxes calculated at checkout.</p>
                         <div className="mt-6">
-                          <a
-                            href="#"
+                          <Link
+                            href="/checkout"
+                            onClick={()=> setOpen(false)}
                             className="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
                           >
                             Checkout
-                          </a>
+                          </Link>
                         </div>
                         <div className="mt-6 flex justify-center text-center text-sm text-gray-500">
                           <p>
